79adb7c75406bed37252c49c92023836244c0889
Danny Marshall <dannyhbmarshall@gmail.com>
2024-09-27 15:52:45 -0400

    Initializing

---
+++ NightlyBudgetApp/NightlyBudgetApp/NightlyBudgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightlyBudgetAppApp.swift
+//  NightlyBudgetApp
+//
+//  Created by Danny on 9/27/24.
+//
+
+import SwiftUI
+
+@main
+struct NightlyBudgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NightlyBudgetApp/NightlyBudgetAppTests/NightlyBudgetAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  NightlyBudgetAppTests.swift
+//  NightlyBudgetAppTests
+//
+//  Created by Danny on 9/27/24.
+//
+
+import Testing
+@testable import NightlyBudgetApp
+
+struct NightlyBudgetAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
